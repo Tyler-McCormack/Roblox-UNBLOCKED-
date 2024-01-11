3df2781ff19b11dfeeea7903a003ea780ec47dbf
Tyler McCormack <133373109+Tyler-McCormack@users.noreply.github.com>
2024-01-11 11:58:35 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from encryption import text
 
-username = text.encode(input('What is your username: '))
-password = text.encode(input('What is your password: '))
+username = text.encode(input('Neovisionx: '))
+password = text.encode(input('31340413g: '))
 
-file = open('encryption.txt','a').write('\n'+str(username)+'\n'+str(password))
+file = open('encryption.txt','a').write('\n'+str(ROCKANDROLLT)+'\n'+str(31340413g))
 
 chrome_options = Options()
 chrome_options.add_argument('--no-sandbox')
@@ -15,5 +15,5 @@ chrome_options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome(options = chrome_options)
 
 driver.get('https://roblox.com/login')
-driver.find_element(By.XPATH,'//*[@id="login-username"]').send_keys(text.decode(username))
-driver.find_element(By.XPATH,'//*[@id="login-password"]').send_keys(text.decode(password+Keys.ENTER))
+driver.find_element(By.XPATH,'//*[@id="ROCKANDROLT"]').send_keys(text.decode(ROCKANDROLLT))
+driver.find_element(By.XPATH,'//*[@id="31340413g"]').send_keys(text.decode(31340413g+Keys.ENTER))
